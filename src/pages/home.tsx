9082fd458456f5ef4a785dd315e35f7cbf22c15b
Ryanz23 <ariyanandryan@gmail.com>
2025-10-28 11:38:43 +0700

    update: change title

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
       <section className="py-16 px-4 bg-white shadow-[0_-2px_8px_rgba(243,244,246,1)]">
         <div className="max-w-6xl mx-auto">
           <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">
-            Latest Articles
+            Artikel Terbaru
           </h1>
           <div className="grid md:grid-cols-2 gap-8">
             {articles.slice(0, 4).map((article) => (
